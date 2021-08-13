eee426307a0c66560206c18932d95d49ea8dedbb
sean-blessing <snblessing@gmail.com>
2021-08-13 14:04:07 -0400

    moved credit-list to proper folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
-import { MovieListComponent } from './feature/movie/movie-list/movie-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreditListComponent } from './feature/actor/credit-list/credit-list.component';
-import { ActorListComponent } from './feature/actor/actor-list/actor-list.component';
+import { MovieListComponent } from './feature/movie/movie-list/movie-list.component';
 import { MovieCreateComponent } from './feature/movie/movie-create/movie-create.component';
-import { MovieDetailComponent } from './feature/movie/movie-detail/movie-detail.component';
 import { MovieEditComponent } from './feature/movie/movie-edit/movie-edit.component';
+import { MovieDetailComponent } from './feature/movie/movie-detail/movie-detail.component';
+import { ActorListComponent } from './feature/actor/actor-list/actor-list.component';
+import { CreditListComponent } from './feature/credit/credit-list/credit-list.component';
 import { CreditCreateComponent } from './feature/credit/credit-create/credit-create.component';
-import { CreditDetailComponent } from './feature/credit/credit-detail/credit-detail.component';
 import { CreditEditComponent } from './feature/credit/credit-edit/credit-edit.component';
+import { CreditDetailComponent } from './feature/credit/credit-detail/credit-detail.component';
 import { UserListComponent } from './feature/user/user-list/user-list.component';
 import { UserCreateComponent } from './feature/user/user-create/user-create.component';
-import { UserDetailComponent } from './feature/user/user-detail/user-detail.component';
 import { UserEditComponent } from './feature/user/user-edit/user-edit.component';
+import { UserDetailComponent } from './feature/user/user-detail/user-detail.component';
 import { UserLoginComponent } from './feature/user/user-login/user-login.component';
 
 const routes: Routes = [
@@ -20,17 +20,17 @@ const routes: Routes = [
   { path: 'user-login', component: UserLoginComponent},
   { path: 'user-list', component: UserListComponent},
   { path: 'user-create', component: UserCreateComponent},
-  { path: 'user-detail/:id', component: UserDetailComponent},
   { path: 'user-edit/:id', component: UserEditComponent},
+  { path: 'user-detail/:id', component: UserDetailComponent},
   { path: 'movie-list', component: MovieListComponent},
   { path: 'movie-create', component: MovieCreateComponent},
-  { path: 'movie-detail/:id', component: MovieDetailComponent},
   { path: 'movie-edit/:id', component: MovieEditComponent},
+  { path: 'movie-detail/:id', component: MovieDetailComponent},
   { path: 'actor-list', component: ActorListComponent},
   { path: 'credit-list', component: CreditListComponent},
   { path: 'credit-create', component: CreditCreateComponent},
-  { path: 'credit-detail/:id', component: CreditDetailComponent},
   { path: 'credit-edit/:id', component: CreditEditComponent},
+  { path: 'credit-detail/:id', component: CreditDetailComponent},
   { path: '**', component: MovieListComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './core/menu/menu.component';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './feature/movie/movie-list/movie-list.component';
-import { ActorListComponent } from './feature/actor/actor-list/actor-list.component';
-import { CreditListComponent } from './feature/actor/credit-list/credit-list.component';
-import { MenuComponent } from './core/menu/menu.component';
 import { MovieCreateComponent } from './feature/movie/movie-create/movie-create.component';
 import { MovieEditComponent } from './feature/movie/movie-edit/movie-edit.component';
 import { MovieDetailComponent } from './feature/movie/movie-detail/movie-detail.component';
-import { CreditDetailComponent } from './feature/credit/credit-detail/credit-detail.component';
-import { CreditEditComponent } from './feature/credit/credit-edit/credit-edit.component';
+import { ActorListComponent } from './feature/actor/actor-list/actor-list.component';
+import { CreditListComponent } from './feature/credit/credit-list/credit-list.component';
 import { CreditCreateComponent } from './feature/credit/credit-create/credit-create.component';
+import { CreditEditComponent } from './feature/credit/credit-edit/credit-edit.component';
+import { CreditDetailComponent } from './feature/credit/credit-detail/credit-detail.component';
 import { UserListComponent } from './feature/user/user-list/user-list.component';
 import { UserLoginComponent } from './feature/user/user-login/user-login.component';
 import { UserCreateComponent } from './feature/user/user-create/user-create.component';
@@ -24,21 +24,21 @@ import { UserEditComponent } from './feature/user/user-edit/user-edit.component'
 @NgModule({
   declarations: [
     AppComponent,
-    MovieListComponent,
-    ActorListComponent,
-    CreditListComponent,
     MenuComponent,
+    MovieListComponent,
     MovieCreateComponent,
     MovieEditComponent,
     MovieDetailComponent,
-    CreditDetailComponent,
-    CreditEditComponent,
+    ActorListComponent,
+    CreditListComponent,
     CreditCreateComponent,
+    CreditEditComponent,
+    CreditDetailComponent,
     UserListComponent,
     UserLoginComponent,
     UserCreateComponent,
-    UserDetailComponent,
-    UserEditComponent
+    UserEditComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
