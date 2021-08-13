d9bd4ef6d75ad1014a4d7fcd6df126625cd4e25f
sean-blessing <snblessing@gmail.com>
2021-08-13 14:13:54 -0400

    added actor create, edit, detail

---
+++ src/app/feature/actor/actor-create/actor-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actor-create',
+  templateUrl: './actor-create.component.html',
+  styleUrls: ['./actor-create.component.css']
+})
+export class ActorCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
