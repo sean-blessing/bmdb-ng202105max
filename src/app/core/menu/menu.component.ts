5e39bf9a5b367e741371df432857def816742921
myusername <myemail>
2021-08-17 09:57:19 -0400

    added instruction for dynamically creating menu based on authority

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,3 +1,4 @@
+import { SystemService } from 'src/app/service/system.service';
 import { MenuItem } from './../../model/menu-item.class';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,16 +11,27 @@ export class MenuComponent implements OnInit {
 
   menuItems: MenuItem[] = [];
 
-  constructor() { }
+  constructor(
+    private sysSvc: SystemService
+  ) { }
 
   ngOnInit(): void {
+    //Note:  'Logout' menu item dislpay assumes you have
+    // checkLogin() function added on every component
     this.menuItems = [
       new MenuItem("User", "/user-list", "User List"),
       new MenuItem("Movie", "/movie-list", "Movie List"),
       new MenuItem("Actor", "/actor-list", "Actor List"),
       new MenuItem("Credit", "/credit-list", "Credit List"),
-      new MenuItem("Login", "/user-login", "User Login")
+      new MenuItem("Logout", "/user-login", "User Login")
     ];
+
+    //is user reviewer?  if so, add Review menu component
+
+    // is user logged in?
+    // displayStr = (if sysvsc.id == 0) ? "Login" : "Logout"
+    // this.menuItems.push(new MenuItem(dislpayStr, "/user-login", "User Login"));
+
   }
 
 }
